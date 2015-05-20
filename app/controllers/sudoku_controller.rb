55cd5739eb38b4af5ed2a411cc866decb459dc40
Ajun <ajunpreet@gmail.com>
2015-05-20 02:54:45 -0400

    add hard and evil example

--- app/controllers/sudoku_controller.rb
+++ app/controllers/sudoku_controller.rb
@@ -14,6 +14,7 @@ class SudokuController < ApplicationController
 			end
 			puzzle.board.values[row.to_i][column.to_i] = value
 		end
+
 		puzzle.solve
 		@board = puzzle.board.values
 	end
@@ -40,5 +41,29 @@ class SudokuController < ApplicationController
 					 [nil, 8, nil, nil, 9, 3, nil, nil, 2],
 					 [6, 9, nil, nil, 5, nil, 3, nil, nil],
 					 [nil, 3, nil, nil, 4, 8, nil, nil, nil]]
+	end
+
+	def hard
+		@values = [[6, nil, nil, 8, nil, 9, nil, nil, nil],
+					 [nil, nil, 5, nil, nil, 7, nil, 8, 6],
+					 [nil, 7, nil, nil, nil, nil, nil, nil, nil],
+					 [nil, nil, nil, 4, nil, 1, 3, nil, 7],
+					 [8, nil, 1, nil, nil, nil, 5, nil, 4],
+					 [7, nil, 9, 2, nil, 5, nil, nil, nil],
+					 [nil, nil, nil, nil, nil, nil, nil, 4, nil],
+					 [1, 8, nil, 5, nil, nil, 6, nil, nil],
+					 [nil, nil, nil, 3, nil, 4, nil, nil, 5]]
+	end
+
+	def evil
+		@values = [[1, nil, nil, nil, nil, nil, nil, nil, nil],
+					 [7, nil, nil, nil, nil, 8, 1, nil, 2],
+					 [nil, 6, 3, nil, 5, nil, nil, nil, nil],
+					 [nil, 7, nil, 3, 9, nil, nil, nil, nil],
+					 [nil, nil, 5, 8, nil, 4, 6, nil, nil],
+					 [nil, nil, nil, nil, 2, 5, nil, 4, nil],
+					 [nil, nil, nil, nil, 1, nil, 8, 7, nil],
+					 [2, nil, 8, 9, nil, nil, nil, nil, 3],
+					 [nil, nil, nil, nil, nil, nil, nil, nil, 6]]
 	end
 end
